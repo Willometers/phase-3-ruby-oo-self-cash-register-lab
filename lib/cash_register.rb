055e2afcb27c4ce7b30f1d575421c6e39963a787
BillyMiles <williammiles201@gmail.com>
2021-08-25 12:19:13 -0400

    commit tree 4d810a91cba298b7a896436bdd01c1cb382f78f2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
 
+    attr_accessor :total, :discount, :items
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def total 
+        @total
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total + price * quantity
+    end
+    
+    def apply_discount
+        @discount
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+
+    end
+    
+end
